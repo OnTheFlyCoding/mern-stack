a4b69ec2d54da0b1249dbd9af007ac4f8320d643
Giovanni Gonzalez <createdtyler@gmail.com>
2024-08-26 19:35:12 -0400

    edit: server.js, added express, set configuration env var through dotenv, call app function to listen on designated port

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log('Hello World!!!');
+//add backend framework
+const express = require('express')
+//Set env variables
+const  dotenv = require('dotenv').config()
+//Designate port # to server
+const port = 5000
+//Initialize express
+const app = express()
+//Call app function to listen on port
+app.listen(port, () => console.log(`Server started on port: ${port}`))
