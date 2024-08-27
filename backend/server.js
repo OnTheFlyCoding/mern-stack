d288e303c0749196c7bc3a5fdd6a32f915957ef1
Giovanni Gonzalez <createdtyler@gmail.com>
2024-08-27 19:13:40 -0400

    Brought in errorHandler from errorMiddleware. app.use(errorHandler) calls the export.

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,14 @@ const  dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
 //Initialize express
 const app = express()
+//Bring in error handlers
+const {errorHandler} = require('./middleware/errorMiddleware')
+//<---Add middleware--->
+app.use(express.json())
+app.use(express.urlencoded({extended : false}))
+
 //access express router to respond to the request coming from the front-end
 app.use('/api/goals', require('./routes/goalRoutes'))
+app.use(errorHandler)
 //Call app function to listen on port
 app.listen(port, () => console.log(`Server started on port: ${port}`))
