3ed4aaf306d4de14dbffb6e9620208ff933b5f32
Giovanni Gonzalez <createdtyler@gmail.com>
2024-08-27 19:26:11 -0400

    setting up async functions for when we incorporate mongooseDb

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -7,7 +7,7 @@ const getGoal = ((req,res)=>{
 // @desc Set Goals
 // @route POST /api/goals
 // @access Private
-const setGoal = ((req,res)=>{
+const setGoal = async (req,res)=>{
     //Check if user doesnt send body message, if so send error
     if(!req.body.text){
         res.status(400)
@@ -15,19 +15,19 @@ const setGoal = ((req,res)=>{
         
     }
     res.status(200).json({message:'Set Goals'})
-})
+}
 // @desc Update Goals
 // @route PUT /api/goals/:id
 // @access Private
-const updateGoal = ((req,res)=>{
+const updateGoal = async (req,res)=>{
     res.status(200).json({message : `Update goal ${req.params.id}`})
-})
+}
 // @desc Delete Goals
 // @route DELETE /api/goals/:id
 // @access Private
-const deleteGoal = ((req,res)=>{
+const deleteGoal = async (req,res)=>{
     res.status(200).json({message : `Delete goal ${req.params.id}`})
-})
+}
 
 //export your functionality to get routed back to the frontend
 module.exports = {
