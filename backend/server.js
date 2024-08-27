54485244f1bb8ad1280227be75499b864a317cda
Giovanni Gonzalez <createdtyler@gmail.com>
2024-08-27 19:22:14 -0400

    added comment on line 17

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ app.use(express.urlencoded({extended : false}))
 
 //access express router to respond to the request coming from the front-end
 app.use('/api/goals', require('./routes/goalRoutes'))
+//runs after to catch the errors sent to the server
 app.use(errorHandler)
 //Call app function to listen on port
 app.listen(port, () => console.log(`Server started on port: ${port}`))
