1117272ea67e4fb65dd2ea7648cee75e9ffdb5d9
Giovanni Gonzalez <createdtyler@gmail.com>
2024-08-27 20:10:16 -0400

    added connection to mongodb

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -1,13 +1,15 @@
+const asyncHandler = require('express-async-handler')
+
 // @desc Get Goals
 // @route GET /api/goals
 // @access Private
-const getGoal = ((req,res)=>{
+const getGoal = asyncHandler(async (req,res)=>{
     res.status(200).json({message : 'Get Goals'})
 })
 // @desc Set Goals
 // @route POST /api/goals
 // @access Private
-const setGoal = async (req,res)=>{
+const setGoal = asyncHandler(async (req,res)=>{
     //Check if user doesnt send body message, if so send error
     if(!req.body.text){
         res.status(400)
@@ -15,19 +17,19 @@ const setGoal = async (req,res)=>{
         
     }
     res.status(200).json({message:'Set Goals'})
-}
+})
 // @desc Update Goals
 // @route PUT /api/goals/:id
 // @access Private
-const updateGoal = async (req,res)=>{
+const updateGoal = asyncHandler(async (req,res)=>{
     res.status(200).json({message : `Update goal ${req.params.id}`})
-}
+})
 // @desc Delete Goals
 // @route DELETE /api/goals/:id
 // @access Private
-const deleteGoal = async (req,res)=>{
+const deleteGoal = asyncHandler(async (req,res)=>{
     res.status(200).json({message : `Delete goal ${req.params.id}`})
-}
+})
 
 //export your functionality to get routed back to the frontend
 module.exports = {
