a6e3834cd47e17b73b07169818b2c513ab5b935b
Giovanni Gonzalez <createdtyler@gmail.com>
2024-09-02 15:17:06 -0400

    vnsdfsdjf

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -31,13 +31,24 @@ const setGoal = asyncHandler(async (req,res)=>{
 // @route PUT /api/goals/:id
 // @access Private
 const updateGoal = asyncHandler(async (req,res)=>{
-    res.status(200).json({message : `Update goal ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+    if(!goal){
+        res.status(400)
+        throw new Error ('Goal not found')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+
+    })
+    res.status(200).json(updatedGoal)
 })
 // @desc Delete Goals
 // @route DELETE /api/goals/:id
 // @access Private
 const deleteGoal = asyncHandler(async (req,res)=>{
-    res.status(200).json({message : `Delete goal ${req.params.id}`})
+
+    res.status(200).json({id : req.params.id})
 })
 
 //export your functionality to get routed back to the frontend
